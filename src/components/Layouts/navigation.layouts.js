6b5c3884e96d88d90d5669dfe0150bae77b54804
Marzuki <marjukiiahmad@gmail.com>
2023-02-27 06:01:10 +0700

    update responsive

--- src/components/Layouts/navigation.layouts.js
+++ src/components/Layouts/navigation.layouts.js
@@ -1,23 +1,34 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
 
 const Navigation = () => {
-    const pathName = window.location.pathname
-    return (
-      <Navbar bg="dark" variant="dark" expand="lg" >
-      <Container>
-        <Navbar.Brand href="/">ReactJS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link style={ (pathName === "/") ? { color: "black", backgroundColor: "white", fontWeight: "500", borderRadius: "5px"} : {} } href="/"> Home</Nav.Link>
-            <Nav.Link style={ (pathName === "/albums") ? { color: "black", backgroundColor: "white", fontWeight: "500", borderRadius: "5px"} : {} } href="/albums">Albums</Nav.Link>
-            <Nav.Link style={ (pathName === "/posts") ? { color: "black", backgroundColor: "white", fontWeight: "500", borderRadius: "5px"} : {} } href="/posts">Posts</Nav.Link>
-            <Nav.Link style={ (pathName === "/photos") ? { color: "black", backgroundColor: "white", fontWeight: "500", borderRadius: "5px"} : {} } href="/photos">Photos</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    )
-}
+    const pathName = window.location.pathname;
 
-export default Navigation
+    console.log(pathName);
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">ReactJS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Link style={pathName === "/" ? { color: "white", borderBottom: "2px solid white" } : {}} href="/">
+                            {" "}
+                            Home
+                        </Nav.Link>
+                        <Nav.Link style={pathName === "/albums" ? { color: "white", borderBottom: "2px solid white" } : {}} href="/albums">
+                            Albums
+                        </Nav.Link>
+                        <Nav.Link style={pathName === "/posts" ? { color: "white", borderBottom: "2px solid white" } : {}} href="/posts">
+                            Posts
+                        </Nav.Link>
+                        <Nav.Link style={pathName === "/photos" ? { color: "white", borderBottom: "2px solid white" } : {}} href="/photos">
+                            Photos
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Navigation;
